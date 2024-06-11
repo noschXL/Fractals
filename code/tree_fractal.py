727b66108a40c814fcebea7a0f140cc31ab7cdf7
noschXL <131900039+noschXL@users.noreply.github.com>
2024-06-11 11:20:48 +0200

    Finish tree_fractal.py

--- code/tree_fractal.py
+++ code/tree_fractal.py
@@ -1,7 +1,55 @@
-import PointWalker as PointWalker
-from settings import *
+import PointWalker #type:ignore
+import math
+from settings import * #type:ignore
+
+alpha = 53
+beta = 90 - alpha
 
 def start(n: int):
-    l = width / (3**n)
-    points = []
-    walker = PointWalker.Point()
+    string = makestr(n)
+    ret =  followstr(string, n)
+    print(string)
+    return ret
+    
+    
+    
+def makestr(n):
+    start = "FFLOBRROBL"
+    rules = {
+            "O": "FFLOBRROBL",
+            "F": "FF",
+            "B": "BB"
+            }
+    
+    newstr = start
+    for _ in range(n):
+        newstr = ""
+        for char in start:
+            try:
+                newstr += rules[char]
+            except KeyError:
+                newstr += char
+
+        start = newstr
+
+    return start
+
+def followstr(string, n) -> list[tuple]:
+    walker = PointWalker.Point((width / 2, 599), -90)
+    l = height / (2 ** (n + 2))
+    points = [walker.get_pos()]
+    for char in string:
+        if char == "F" or char == "O":
+            points.append(walker.forward(l))
+        elif char == "B":
+            points.append(walker.backward(l))
+        elif char == "L":
+            walker.left(45)
+        elif char == "R":
+            walker.right(45)
+            
+    return points
+
+if __name__ == "__main__":
+    string = makestr(1)
+    
